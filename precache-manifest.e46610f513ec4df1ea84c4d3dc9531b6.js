516bf985e309ba0f3f06adc2b856d5808d5a4c8d
rinqtmith <rinqtf@gmail.com>
2020-06-16 08:59:23 +0300

    Updates

---
+++ precache-manifest.e46610f513ec4df1ea84c4d3dc9531b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399b5aa9d4335a3a15462ca47de14349",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "a5b66d15127a2844f364",
+    "url": "/monsters/static/css/main.89209a8b.chunk.css"
+  },
+  {
+    "revision": "80de6e261c3a1f36230c",
+    "url": "/monsters/static/js/2.05463ef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.05463ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5b66d15127a2844f364",
+    "url": "/monsters/static/js/main.4b2b925b.chunk.js"
+  },
+  {
+    "revision": "5520c3e6235208f1eea1",
+    "url": "/monsters/static/js/runtime-main.59ed3f86.js"
+  }
+]);
